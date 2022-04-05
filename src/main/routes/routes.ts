43137941df3600080b13576766bb93b8577c73f2
Bruno Alves <bruno@sthorm.io>
2022-04-05 05:58:52 -0300

    feat: add route to check if api is available

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -5,6 +5,8 @@ import { specificationRouter } from './specification.routes';
 
 const appRouter = Router();
 
+appRouter.get('/', (_, res) => res.json({ message: 'API is available' }));
+
 appRouter.use('/categories', categoryRouter);
 appRouter.use('/specifications', specificationRouter);
 
