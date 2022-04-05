810a1f1658a08cec4736811863af0f2df57e2bf4
Bruno Alves <bruno@sthorm.io>
2022-04-05 00:28:26 -0300

    add specification router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { categoryRouter } from './categories.routes';
+import { specificationRouter } from './specification.routes';
 
 const appRouter = Router();
 
 appRouter.use('/categories', categoryRouter);
+appRouter.use('/specification', specificationRouter);
 
 export { appRouter };
