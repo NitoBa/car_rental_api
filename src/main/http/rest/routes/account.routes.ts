a75daabcaec9be1a1f5678dc2977d728da79e0d9
Bruno Alves <bruno@sthorm.io>
2022-04-08 13:28:08 -0300

    add route to authenticate user

--- src/main/http/rest/routes/account.routes.ts
+++ src/main/http/rest/routes/account.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
 import { adaptRouter } from '../adapters/express-router-adapter';
-import { createCreateUserController } from '../factories/account-factory';
+import {
+  createAuthenticateUserController,
+  createCreateUserController,
+} from '../factories/account-factory';
 
 const accountRouter = Router();
 
 accountRouter.post('/users', adaptRouter(createCreateUserController()));
+accountRouter.post(
+  '/users/session',
+  adaptRouter(createAuthenticateUserController())
+);
 
 export { accountRouter };
