0a1badb582d05982ac3379a5eec6b0688a2a5a19
Bruno Alves <bruno@sthorm.io>
2022-04-08 13:20:52 -0300

    add authenticate controller

---
+++ src/presentation/controllers/authenticate-user-controller.ts
@@ -0,0 +1,31 @@
+import { LoginEmailPasswordUsecase } from '../../application/usecases/login-email-password-usecase';
+import { badRequest, ok } from '../helpers/http-response-helper';
+import { IController } from '../interfaces/controller';
+import { HttpResponse } from '../interfaces/http-response';
+
+type AuthenticateUserRequest = {
+  body: {
+    email: string;
+    password: string;
+  };
+};
+
+export class AuthenticateUser implements IController<AuthenticateUserRequest> {
+  constructor(
+    private loginWithEmailPasswordUsecase: LoginEmailPasswordUsecase
+  ) {}
+
+  async handle({ body }: AuthenticateUserRequest): Promise<HttpResponse> {
+    try {
+      const { email, password } = body;
+      const token = await this.loginWithEmailPasswordUsecase.execute(
+        email,
+        password
+      );
+
+      return ok(token);
+    } catch (error) {
+      return badRequest(error);
+    }
+  }
+}
