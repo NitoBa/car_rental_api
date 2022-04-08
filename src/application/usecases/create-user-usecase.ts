0a3a28780638755c5a3c4a5902d073219ab722f1
Bruno Alves <bruno@sthorm.io>
2022-04-08 11:16:32 -0300

    implements usecase to create user

---
+++ src/application/usecases/create-user-usecase.ts
@@ -0,0 +1,44 @@
+import { CreateUserDTO } from '../dtos/create-user-dto';
+import { IEncryptRepository } from '../repositories/iencrypt-repository';
+import { IUsersRepository } from '../repositories/iusers-repository';
+
+export class CreateUserUsecase {
+  constructor(
+    private usersRepository: IUsersRepository,
+    private encryptRepository: IEncryptRepository
+  ) {}
+
+  async execute(input: CreateUserDTO): Promise<void> {
+    const { name, username, email, password, driver_license } = input;
+
+    if (!name || !username || !email || !password || !driver_license) {
+      throw new Error('Missing fields');
+    }
+
+    const userAlreadyExistsEmail = this.usersRepository.findUserByEmail(email);
+
+    if (userAlreadyExistsEmail) {
+      throw new Error('User already exists with this email');
+    }
+
+    const userAlreadyExistsUsername =
+      await this.usersRepository.findUserByUsername(username);
+
+    if (userAlreadyExistsUsername) {
+      throw new Error('User already exists with this username');
+    }
+
+    // TODO: Validate driver license
+
+    // TODO: hash password
+    const passwordHashed = await this.encryptRepository.encrypt(password);
+
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      password: passwordHashed,
+      driver_license,
+    });
+  }
+}
