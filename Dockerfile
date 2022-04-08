ce5f34097a134254da1dbdf274291218bb87e4a0
Bruno Alves <bruno@sthorm.io>
2022-04-07 22:44:23 -0300

    add docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:16.14.2-alpine
+
+WORKDIR /usr/src/app
+
+COPY package.json ./
+
+RUN npm install
+
+COPY . .
+
+ARG PORT=3333
+
+ENV PORT=$PORT
+EXPOSE $PORT
+
+CMD ["npm", "run", "dev"]
+
+
--- src/main/http/rest/server.ts
+++ src/main/http/rest/server.ts
@@ -1,3 +1,5 @@
 import { app } from './app';
 
-app.listen(3333, () => console.log('Server listening on port 3333!'));
+const PORT = process.env.PORT ?? 3333;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
