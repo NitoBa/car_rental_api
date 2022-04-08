7a396d7496af22e80b6dbd3dde7a4cdf48877ffa
Bruno Alves <bruno@sthorm.io>
2022-04-08 14:24:40 -0300

    use middleware in routes

--- src/main/http/rest/routes/categories.routes.ts
+++ src/main/http/rest/routes/categories.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
+import { adaptMiddleware } from '../adapters/express-middleware-adapter';
 import { adaptRouter } from '../adapters/express-router-adapter';
+import { createEnsureAuthenticatedMiddleware } from '../factories/account-factory';
 import {
   createCategoryController,
   createGetAllCategoriesController,
@@ -12,7 +14,11 @@ import { handleUploadFile } from '../middlewares/handle-upload-file';
 const categoryRouter = Router();
 
 categoryRouter.get('/', adaptRouter(createGetAllCategoriesController()));
-categoryRouter.post('/', adaptRouter(createCategoryController()));
+categoryRouter.post(
+  '/',
+  adaptMiddleware(createEnsureAuthenticatedMiddleware()),
+  adaptRouter(createCategoryController())
+);
 categoryRouter.put('/:id', adaptRouter(updateAllCategoryController()));
 
 categoryRouter.post(
