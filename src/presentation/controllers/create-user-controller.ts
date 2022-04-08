969cbd14c710d33f029681509dd3d318be3e47b4
Bruno Alves <bruno@sthorm.io>
2022-04-08 11:21:42 -0300

    implements create user controller

---
+++ src/presentation/controllers/create-user-controller.ts
@@ -0,0 +1,23 @@
+import { CreateUserDTO } from '../../application/dtos/create-user-dto';
+import { CreateUserUsecase } from '../../application/usecases/create-user-usecase';
+import { badRequest, created } from '../helpers/http-response-helper';
+import { IController } from '../interfaces/controller';
+import { HttpResponse } from '../interfaces/http-response';
+
+type CreateUserRequest = {
+  body: CreateUserDTO;
+};
+
+export class CreateUserController implements IController<CreateUserRequest> {
+  constructor(private createUserUsecase: CreateUserUsecase) {}
+
+  async handle({ body }: CreateUserRequest): Promise<HttpResponse> {
+    try {
+      await this.createUserUsecase.execute(body);
+
+      return created();
+    } catch (error) {
+      return badRequest(error);
+    }
+  }
+}
