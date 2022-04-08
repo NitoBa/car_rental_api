0a8fc98d7631daa655a8cb6772ee10894685a078
Bruno Alves <bruno@sthorm.io>
2022-04-08 13:23:45 -0300

    create factory function to return authenticate user controller

--- src/main/http/rest/factories/account-factory.ts
+++ src/main/http/rest/factories/account-factory.ts
@@ -1,13 +1,27 @@
 import { CreateUserUsecase } from '../../../../application/usecases/create-user-usecase';
+import { LoginEmailPasswordUsecase } from '../../../../application/usecases/login-email-password-usecase';
 import { prisma } from '../../../../external/database/prisma-service';
 import { EncryptRepositoryBcrypt } from '../../../../infra/repositories/encrypt-repository-bcrypt';
+import { JwtRepository } from '../../../../infra/repositories/jwt-repository';
 import { UsersRepositoryPrisma } from '../../../../infra/repositories/users-repository-prisma';
+import { AuthenticateUserController } from '../../../../presentation/controllers/authenticate-user-controller';
 import { CreateUserController } from '../../../../presentation/controllers/create-user-controller';
 
 const usersRepository = new UsersRepositoryPrisma(prisma);
 const encryptRepository = new EncryptRepositoryBcrypt();
+const jwtRepository = new JwtRepository();
 
 export const createCreateUserController = () => {
   const usecase = new CreateUserUsecase(usersRepository, encryptRepository);
   return new CreateUserController(usecase);
+};
+
+export const createAuthenticateUserController = () => {
+  const usecase = new LoginEmailPasswordUsecase(
+    usersRepository,
+    encryptRepository,
+    jwtRepository
+  );
+
+  return new AuthenticateUserController(usecase);
 };
