ac8be07b914735287d8e3ec471af9cf5d57e7a71
Bruno Alves <bruno@sthorm.io>
2022-04-18 19:34:39 -0300

    add test to create category usecase

---
+++ src/application/usecases/create-category-usecase.spec.ts
@@ -0,0 +1,53 @@
+import { Category } from '../../domain/entities/category';
+import { InMemoryCategoryRepository } from '../../tests/repositories/in-memory-category-repository';
+import { CreateCategoryUsecase } from './create-category-usecase';
+
+const makeSut = () => {
+  const repository = new InMemoryCategoryRepository();
+  const sut = new CreateCategoryUsecase(repository);
+  return {
+    sut,
+    repository,
+  };
+};
+
+describe('Create Category Usecase', () => {
+  it('should not be able to create a category if already exists', () => {
+    const { sut, repository } = makeSut();
+    const category: Category = {
+      id: 'id',
+      name: 'name',
+      description: 'description',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    repository.categories.push(category);
+
+    const result = sut.execute({
+      name: 'name',
+      description: 'description',
+    });
+
+    expect(result).rejects.toThrow('Category already exists.');
+  });
+
+  it('should be able to create a category', async () => {
+    const { sut, repository } = makeSut();
+
+    const result = await sut.execute({
+      name: 'name',
+      description: 'description',
+    });
+
+    expect(result).toBeUndefined();
+    expect(repository.categories.length).toBe(1);
+    expect(repository.categories).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: 'name',
+        }),
+      ])
+    );
+  });
+});
---
+++ src/tests/repositories/in-memory-category-repository.ts
@@ -0,0 +1,29 @@
+import { CreateCategoryDTO } from '../../application/dtos/create-category-dto';
+import { UpdateCategoryDTO } from '../../application/dtos/update-category-dto';
+import { ICategoryRepository } from '../../application/repositories/icategory-repository';
+import { Category } from '../../domain/entities/category';
+
+export class InMemoryCategoryRepository implements ICategoryRepository {
+  categories: Category[] = [];
+  async create(category: CreateCategoryDTO): Promise<void> {
+    const newCategory = new Category();
+    Object.assign(newCategory, category);
+
+    this.categories.push(newCategory);
+  }
+  update(category: UpdateCategoryDTO): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  findById(id: string): Promise<Category> {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Promise<Category[]> {
+    throw new Error('Method not implemented.');
+  }
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name);
+  }
+}
