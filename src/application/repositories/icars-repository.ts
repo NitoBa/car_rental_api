ea6a294183ddd04d739ff7d7ed69c324f444ae3d
Bruno Alves <bruno@sthorm.io>
2022-04-19 20:13:35 -0300

    implements cars repository prisma

--- src/application/repositories/icars-repository.ts
+++ src/application/repositories/icars-repository.ts
@@ -1,5 +1,5 @@
+import { Car } from '../../domain/entities/car';
 import { CreateCarDTO } from '../dtos/create-car-dto';
-import { Car } from '../usecases/createCar/create-car-usecase.spec';
 
 export interface ICarsRepository {
   findByPlate(plate: string): Promise<Car | undefined>;
---
+++ src/infra/repositories/cars-repository-prisma.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from '@prisma/client';
+
+import { CreateCarDTO } from '../../application/dtos/create-car-dto';
+import { ICarsRepository } from '../../application/repositories/icars-repository';
+import { Car } from '../../domain/entities/car';
+
+export class CarsRepositoryPrisma implements ICarsRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  async findByPlate(plate: string): Promise<Car> {
+    const car = await this.prisma.car.findUnique({
+      where: {
+        licensePlate: plate,
+      },
+      include: {
+        category: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    if (!car) return null;
+
+    const newCar = new Car();
+
+    Object.assign(newCar, {
+      id: car.id,
+      name: car.name,
+      description: car.description,
+      fineAmount: car.fineAmount,
+      dailyRate: car.dailyRate,
+      licensePlate: car.licensePlate,
+      category: car.category.name,
+      brand: car.brand,
+    });
+
+    return newCar;
+  }
+  async create(input: CreateCarDTO): Promise<void> {
+    await this.prisma.car.create({
+      data: {
+        ...input,
+      },
+    });
+  }
+}
