80954891a7c8496eba07d7b33b835be25ede0485
Bruno Alves <bruno@sthorm.io>
2022-04-20 11:18:13 -0300

    implements find by specification and car id on prisma repository

--- src/infra/repositories/specifications-car-repository.ts
+++ src/infra/repositories/specifications-car-repository.ts
@@ -7,6 +7,22 @@ export class SpecificationsCarsRepository
   implements ISpecificationCarRepository
 {
   constructor(private prisma: PrismaClient) {}
+  async findBySpecificationIdAndCarId(
+    input: CreateSpecificationCarDTO
+  ): Promise<boolean> {
+    const { carId, specificationId } = input;
+
+    const specificationCar = await this.prisma.specificationsCars.findFirst({
+      where: {
+        AND: {
+          carId,
+          specificationId,
+        },
+      },
+    });
+
+    return !!specificationCar;
+  }
   async createSpecificationCar(
     input: CreateSpecificationCarDTO
   ): Promise<void> {
