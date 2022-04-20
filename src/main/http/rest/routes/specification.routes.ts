64bba9bd427de0b9f244d44bd96037fc3dd42380
Bruno Alves <bruno@sthorm.io>
2022-04-20 11:24:32 -0300

    add admin middleware to create specifications car

--- src/main/http/rest/routes/specification.routes.ts
+++ src/main/http/rest/routes/specification.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
+import { adaptMiddleware } from '../adapters/express-middleware-adapter';
 import { adaptRouter } from '../adapters/express-router-adapter';
+import { createEnsureUserIsAdminMiddleware } from '../factories/cars-factory';
 import {
   createSpecificationCarController,
   createSpecificationController,
@@ -13,6 +15,7 @@ const specificationRouter = Router();
 specificationRouter.post('/', adaptRouter(createSpecificationController()));
 specificationRouter.post(
   '/car',
+  adaptMiddleware(createEnsureUserIsAdminMiddleware()),
   adaptRouter(createSpecificationCarController())
 );
 specificationRouter.get('/', adaptRouter(getAllSpecificationsController()));
