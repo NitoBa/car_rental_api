64828fa68a722a6ed16ed98ccd1c62b8ef0e2e7d
Bruno Alves <bruno@sthorm.io>
2022-04-20 21:29:15 -0300

    add rentals table

---
+++ prisma/migrations/20220421002300_add_rentals_table/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "rentals" (
+    "id" TEXT NOT NULL,
+    "carId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3),
+    "expectReturnDate" TIMESTAMP(3) NOT NULL,
+    "total" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "rentals_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "rentals" ADD CONSTRAINT "rentals_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rentals" ADD CONSTRAINT "rentals_carId_fkey" FOREIGN KEY ("carId") REFERENCES "cars"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
