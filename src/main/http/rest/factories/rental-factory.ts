8d6a3912d549e0247d9b2462bec1f6554d2cd4e3
Bruno Alves <bruno@sthorm.io>
2022-04-21 00:43:36 -0300

    update rental factory

--- src/main/http/rest/factories/rental-factory.ts
+++ src/main/http/rest/factories/rental-factory.ts
@@ -1,10 +1,12 @@
 import { CreateRentalUsecase } from '../../../../application/usecases/createRental/create-rental-usecase';
 import { prisma } from '../../../../external/database/prisma-service';
 import { CarsRepositoryPrisma } from '../../../../infra/repositories/cars-repository-prisma';
+import { HandleDateRepositoryDayJs } from '../../../../infra/repositories/handle-date-repository-dayjs';
 import { RentalRepositoryPrisma } from '../../../../infra/repositories/rental-repository-prisma';
 import { UsersRepositoryPrisma } from '../../../../infra/repositories/users-repository-prisma';
 import { CreateRentalController } from '../../../../presentation/controllers/create-rental-controller';
 
+const handleDateRepository = new HandleDateRepositoryDayJs();
 const carsRepository = new CarsRepositoryPrisma(prisma);
 const usersRepository = new UsersRepositoryPrisma(prisma);
 const rentalRepository = new RentalRepositoryPrisma(prisma);
@@ -13,7 +15,8 @@ export const createRentalController = () => {
   const usecase = new CreateRentalUsecase(
     rentalRepository,
     carsRepository,
-    usersRepository
+    usersRepository,
+    handleDateRepository
   );
 
   return new CreateRentalController(usecase);
