e1c66bd4ad825b6509b1c780ccabd874c1158c22
Bruno Alves <bruno@sthorm.io>
2022-04-21 19:10:20 -0300

    create category controller test integration

--- src/presentation/controllers/createCategory/create-category-controller.test.ts
+++ src/presentation/controllers/createCategory/create-category-controller.test.ts
@@ -1,36 +1,160 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from 'supertest';
 
+import { prisma } from '../../../external/database/prisma-service';
+import { JwtRepository } from '../../../infra/repositories/jwt-repository';
 import { app } from '../../../main/http/rest/app';
 
-describe('Create Category Controller', () => {
-  it('should be able to list all categories', async () => {
-    const response = await request(app).get('/');
+const jwtRepository = new JwtRepository();
 
-    console.log(response.body);
-
-    expect(response.status).toBe(200);
+const authenticate = () => {
+  const token = jwtRepository.sign('user-id', {
+    email: 'usertest@email.com',
+    name: 'name',
+    username: 'username',
   });
 
-  //   it('should return 401 if token not provided', async () => {
-  //     const response = await request(app).post('/categories/').send({
-  //       name: 'category',
-  //       description: 'description category',
-  //     });
+  return token;
+};
 
-  //     expect(response.status).toBe(401);
-  //     expect(response.body).toEqual(
-  //       expect.objectContaining({
-  //         error: 'Token not provided',
-  //       })
-  //     );
-  //   });
-  //   it('should return 201 if category is created', async () => {
-  //     const response = await request(app).post('/categories/').send({
-  //       name: 'category',
-  //       description: 'description category',
-  //     });
+const createUser = async () => {
+  await prisma.user.create({
+    data: {
+      id: 'user-id',
+      email: 'usertest@email.com',
+      name: 'name',
+      username: 'username',
+      password: 'password',
+      driver_license: '12345678',
+    },
+  });
+};
 
-  //     expect(response.status).toBe(201);
-  //   });
+const deleteUser = async () => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: 'usertest@email.com',
+    },
+  });
+
+  if (user) {
+    await prisma.user.delete({
+      where: {
+        email: 'usertest@email.com',
+      },
+    });
+  }
+};
+
+const deleteCategory = async () => {
+  const category = await prisma.category.findFirst({
+    where: {
+      name: 'category',
+    },
+  });
+
+  if (category) {
+    await prisma.category.delete({
+      where: {
+        name: 'category',
+      },
+    });
+  }
+};
+
+const createCategory = async () => {
+  await prisma.category.create({
+    data: {
+      name: 'category',
+      description: 'description',
+    },
+  });
+};
+
+afterEach(async () => {
+  await deleteUser();
+  await deleteCategory();
+});
+
+describe('Create Category Controller', () => {
+  it('should return 401 if token not provided', async () => {
+    const response = await request(app).post('/categories/').send({
+      name: 'category',
+      description: 'description category',
+    });
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'Token not provided',
+      })
+    );
+  });
+
+  it('should return 401 if token expired', async () => {
+    const headers = {
+      authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsIm5hbWUiOiJBZG1pbiBVc2VyIiwidXNlcm5hbWUiOiJhZG1pbiIsImlhdCI6MTY1MDUxMzU4MSwiZXhwIjoxNjUwNTE3MTgxLCJzdWIiOiI0ZDY4MTU0Ny1iZDgwLTQwMDAtOWZlMS04YTg2Y2JhMjdhZjYifQ.OVMnZIJXf2J5gIKQXc8sgiq0Clb5rvL9eeX-hwX10aQ',
+    };
+
+    const body = {
+      name: 'category',
+      description: 'description category',
+    };
+
+    const response = await request(app)
+      .post('/categories/')
+      .set(headers)
+      .send(body);
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'jwt expired',
+      })
+    );
+  });
+
+  it('should return 400 if category already exists', async () => {
+    await createUser();
+    await createCategory();
+
+    const headers = {
+      authorization: `Bearer ${authenticate()}`,
+    };
+
+    const body = {
+      name: 'category',
+      description: 'description category',
+    };
+
+    const response = await request(app)
+      .post('/categories/')
+      .set(headers)
+      .send(body);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      error: 'Category already exists.',
+    });
+  });
+
+  it('should be able to create a new category', async () => {
+    await createUser();
+
+    const headers = {
+      authorization: `Bearer ${authenticate()}`,
+    };
+
+    const body = {
+      name: 'category',
+      description: 'description category',
+    };
+
+    const response = await request(app)
+      .post('/categories/')
+      .set(headers)
+      .send(body);
+
+    expect(response.status).toBe(204);
+  });
 });
